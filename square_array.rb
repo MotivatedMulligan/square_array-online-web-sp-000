1a1c6043d5ca8cd02c35dacf1246907d6305111a
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-04 01:12:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def square_array(number)
+number = [1,2,3]
+return array.each {|number| number**2}
 end
+
+ #new_numbers = [9,10,16,25]
+ #square_array(new_numbers)
