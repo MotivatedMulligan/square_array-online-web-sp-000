a88845e5b343cfa390f04fd4e34de06f58c70f55
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-04 02:39:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
-def square_array(numbers)
+def square_array(array)
+Enumerator.new do |y|
 numbers = [1,2,3]
-numbers.each {|a| a**2}
-
+array.each {|a| y << a**2}
+end
+.take(array.length)
 end
 #[].tap do |a|
 #def square_array(new_numbers)
